d7b9d47d97f9595a1cb3bd66ac1a4497fecf3730
Simon Eason <sie.eason@gmail.com>
2024-07-01 10:44:23 +0800

    Setup initial data bindings for displays

--- main.go
+++ main.go
@@ -4,24 +4,50 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
+
+// Button widget variables
+var (
+	startButton *widget.Button
+	resetButton *widget.Button
+)
+
+// Keeps track of current elapsed seconds
+var time binding.Int
 
 func main() {
 	// Create app
 	app := app.New()
 	mainWindow := app.NewWindow("Pay Stopwatch")
 
+	// Set initial time
+	time = binding.NewInt()
+	time.Set(0)
+
 	// Clock widgets
-	clock := widget.NewLabel("Time: 00:00:00")
+	clock := widget.NewLabelWithData(
+		// Automatically format time from seconds
+		binding.NewSprintf(
+			"Time: %d",
+			time,
+		),
+	)
 	clock.Alignment = fyne.TextAlignCenter
-	payClock := widget.NewLabel("Pay: 0.00")
+	payClock := widget.NewLabelWithData(
+		// Automatically calculate pay from seconds
+		binding.NewSprintf(
+			"Pay: %d",
+			time,
+		),
+	)
 	payClock.Alignment = fyne.TextAlignCenter
 
 	// Start/stop and reset widgets
-	startButton := widget.NewButton("Start", func() {})
-	resetButton := widget.NewButton("Reset", func() {})
+	startButton = widget.NewButton("Start", func() {})
+	resetButton = widget.NewButton("Reset", func() {})
 
 	// Create main layout
 	content := container.NewVBox(
