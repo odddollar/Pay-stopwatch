4e9cdcd290fa6a9d6cbd389ce40823cec7f02389
Simon Eason <sie.eason@gmail.com>
2024-07-01 10:19:25 +0800

    Created widget stubs

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
@@ -11,8 +13,28 @@ func main() {
 	app := app.New()
 	mainWindow := app.NewWindow("Pay Stopwatch")
 
+	// Clock widgets
+	clock := widget.NewLabel("Time: 00:00:00")
+	clock.Alignment = fyne.TextAlignCenter
+	payClock := widget.NewLabel("Pay: 0.00")
+	payClock.Alignment = fyne.TextAlignCenter
+
+	// Start/stop and reset widgets
+	startButton := widget.NewButton("Start", func() {})
+	resetButton := widget.NewButton("Reset", func() {})
+
 	// Create main layout
-	content := container.NewVBox(nil)
+	content := container.NewVBox(
+		clock,
+		payClock,
+		container.NewHBox(
+			layout.NewSpacer(),
+			startButton,
+			layout.NewSpacer(),
+			resetButton,
+			layout.NewSpacer(),
+		),
+	)
 
 	// Create menu bar
 	menu := fyne.NewMainMenu(
@@ -32,9 +54,13 @@ func main() {
 		),
 	)
 
-	// Run app
+	// Set window properties
 	mainWindow.SetContent(content)
 	mainWindow.SetMainMenu(menu)
+	mainWindow.Resize(fyne.NewSize(250, 160))
+	mainWindow.SetFixedSize(true)
+
+	// Run app
 	mainWindow.Show()
 	app.Run()
 }
