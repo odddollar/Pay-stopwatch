8bc5203dd3e6f398d300ec48344057f8b7eab228
Simon Eason <sie.eason@gmail.com>
2024-07-01 10:13:20 +0800

    Created menu stubs

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 )
@@ -10,10 +11,30 @@ func main() {
 	app := app.New()
 	mainWindow := app.NewWindow("Pay Stopwatch")
 
+	// Create main layout
 	content := container.NewVBox(nil)
+
+	// Create menu bar
+	menu := fyne.NewMainMenu(
+		fyne.NewMenu(
+			"File",
+			fyne.NewMenuItem(
+				"Change pay rate",
+				func() {},
+			),
+		),
+		fyne.NewMenu(
+			"Help",
+			fyne.NewMenuItem(
+				"About",
+				func() {},
+			),
+		),
+	)
 
 	// Run app
 	mainWindow.SetContent(content)
+	mainWindow.SetMainMenu(menu)
 	mainWindow.Show()
 	app.Run()
 }
